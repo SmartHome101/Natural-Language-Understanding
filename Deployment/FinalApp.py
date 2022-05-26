a653f30be22b9e40c9a8a123cd82df6ff77693a4
Mohamed Nabil <67477345+MohamedNabill7@users.noreply.github.com>
2022-05-27 00:18:00 +0200

    Updated deploy Natural Language Understanding model on the server by "FastAPI"

--- Deployment/FinalApp.py
+++ Deployment/FinalApp.py
@@ -1,18 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
 
-import sys
-import time
-from pathlib import Path
-
-module_path = str(Path.cwd() / "./")
-
-if module_path not in sys.path:
-    sys.path.append(module_path)
-
-print(module_path)
-
-from utils import  Kitchen , Enterainment , News , Weather , Social , IOT
 from Natural_Language_Understanding import NLU
 
 app = FastAPI()
@@ -25,19 +13,9 @@ def index():
 @app.post('/predict')
 def predict(message):
 
-    mappings = {'Greeting' : Social.greet,
-                'Goodbye'  : Social.goodbye,
-                'Thanks'   : Social.thank,
-                'Joke'     : Enterainment.joke,
-                'PlayMusic': Enterainment.play_music,
-                'Cooking'  : Kitchen.cooking,
-                'Weather'  : Weather.weather,
-                'News'     : News.news,
-                'Iot'      : IOT.smartHome}
-
     res = NLU(message)
-    output = str(mappings[res['Intent']](res))
-    return output 
+
+    return res
     
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1',port=8000)                        
